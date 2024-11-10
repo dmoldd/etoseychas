29289d8df307375e80d4c81d49a3ebfb20e5537e
dmoldd <oldtime_tula@mail.ru>
2024-11-10 22:35:48 +0300

    +

--- source/js/burger-opener.js
+++ source/js/burger-opener.js
@@ -1,17 +1,17 @@
-let navMain = document.querySelector('.header__menu');
-let navToggle = document.querySelector('.header__menu-burger');
+let navMain = document.querySelector('.header');
+let navToggle = document.querySelector('.header__burger');
 
-navMain.classList.remove('header__menu--nojs');
+navMain.classList.remove('header--nojs');
 
 let showOrHideMenu = () => {  
   console.log('s');
 
-  if (navMain.classList.contains('header__menu--closed')) {
-    navMain.classList.remove('header__menu--closed');
-    navMain.classList.add('header__menu--opened');
+  if (navMain.classList.contains('header--closed')) {
+    navMain.classList.remove('header--closed');
+    navMain.classList.add('header--opened');
   } else {
-    navMain.classList.add('header__menu--closed');
-    navMain.classList.remove('header__menu--opened');
+    navMain.classList.add('header--closed');
+    navMain.classList.remove('header--opened');
   }
 }
 
