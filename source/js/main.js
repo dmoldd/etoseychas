71a56811753643fde72e18c485fd5a17ba3e7c97
dmoldd <oldtime_tula@mail.ru>
2024-11-17 22:59:25 +0300

    +

--- source/js/main.js
+++ source/js/main.js
@@ -11,16 +11,32 @@ const createCarParams = (obj) => {
   for (let item in obj) {
     createTitle(item);
     createList(item, obj[item]);
+    
+  }
+}
+
+const sortObj = (obj) => {
+  for (const [key, value] of Object.entries(obj)) {
+    createListItem(key, value);
   }
 }
 
 const createList = (listName, obj) => {
-  let list = document.createElement('p');
+  let list = document.createElement('ul');
   list.classList.add('parameters__item');
-  list.innerHTML = listName;
-  for (let item in obj) {
-    let
-  }
+  list.appendChild(sortObj(obj[item]));
+}
+
+const createListItem = (param1, param2) => {
+  let listItem = document.createElement('li');
+  let itemParam1 = document.createElement('span');
+  itemParam1.innerHTML = param1;
+  let itemParam2 = document.createElement('span');
+  itemParam2.innerHTML = param2;
+  listItem.appendChild(itemParam1);
+  listItem.appendChild(itemParam2);
+  console.log(listItem);
+  return listItem;
 }
 
 const createTitle = (titleName) => {
@@ -30,7 +46,9 @@ const createTitle = (titleName) => {
   mainList.appendChild(listTitle);
 }
 
-createList(123);
+// createList(123);
 createTitle(1234);
 
 createCarParams(oka1111Mt29);
+
+// sortObj(oka1111Mt29[главное]);
