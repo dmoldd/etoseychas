1e59a57b428dd54278e8b69325c077f30ab991ef
dmoldd <oldtime_tula@mail.ru>
2024-11-20 19:54:44 +0300

    +

--- source/js/main.js
+++ source/js/main.js
@@ -3,34 +3,44 @@ import { oka1111Mt29 } from "./params.js";
 
 foo();
 
-const mainList = document.querySelector('.parameters').querySelector('.container');
-
-console.log(mainList);
+const mainContainer = document.querySelector('.parameters').querySelector('.container');
+const blockOfCarParams = document.createElement('div');
+const carPicture = document.createElement('img');
 
 const createCarParams = (obj) => {
+  blockOfCarParams.classList.add('parameters__car');
+  carPicture.classList.add('parameters__car-img');
+  carPicture.src = `./img/parameters/vaz1111graph1.png`;
+  blockOfCarParams.appendChild(carPicture);
   for (let item in obj) {
-    createTitle(item);
     createList(item, obj[item]);
-    
   }
+  mainContainer.appendChild(blockOfCarParams);
 }
 
-const sortObj = (obj) => {
+const sortObj = (obj, list) => {
   for (const [key, value] of Object.entries(obj)) {
-    createListItem(key, value);
+    list.appendChild(createListItem(key, value));
   }
 }
 
-const createList = (listName, obj) => {
+const createList = (titleName, obj) => {
   let list = document.createElement('ul');
   list.classList.add('parameters__item');
-  list.appendChild(sortObj(obj[item]));
+  let listTitle = document.createElement('h3');
+  listTitle.classList.add('parameters__item-title');
+  listTitle.innerHTML = titleName;
+
+  blockOfCarParams.appendChild(list);
+  list.appendChild(listTitle);
+  sortObj(obj, list);
 }
 
 const createListItem = (param1, param2) => {
   let listItem = document.createElement('li');
+  listItem.classList.add('parameters__item-value')
   let itemParam1 = document.createElement('span');
-  itemParam1.innerHTML = param1;
+  itemParam1.innerHTML = `${param1}: `;
   let itemParam2 = document.createElement('span');
   itemParam2.innerHTML = param2;
   listItem.appendChild(itemParam1);
@@ -39,16 +49,4 @@ const createListItem = (param1, param2) => {
   return listItem;
 }
 
-const createTitle = (titleName) => {
-  let listTitle = document.createElement('h3');
-  listTitle.classList.add('parameters__title');
-  listTitle.innerHTML = titleName;
-  mainList.appendChild(listTitle);
-}
-
-// createList(123);
-createTitle(1234);
-
 createCarParams(oka1111Mt29);
-
-// sortObj(oka1111Mt29[главное]);
